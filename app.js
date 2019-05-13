071af62f59e2322eebe023fa842759e6bb5a2e2f
Gil <gils@neat-tech.com>
2019-05-13 18:01:38 +0300

    .

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const cameraView = document.querySelector("#camera--view"),
 // Access the device camera and stream to cameraView
 function cameraStart() {
     
-    navigator.mediaDevices.
+    navigator.mediaDevices
         .getUserMedia(constraints)
         .then(function(stream) {
             track = stream.getTracks()[0];
